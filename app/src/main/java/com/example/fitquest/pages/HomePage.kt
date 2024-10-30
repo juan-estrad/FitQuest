228c2374732c1f2aea8c81048190a2213f7a8f13
NIKTRAN <nicktrantrituan@gmail.com>
2024-10-29 22:55:14 -0400

    Slight top and bottom bar refining + Putting ForYouPage into Home page

--- app/src/main/java/com/example/fitquest/pages/HomePage.kt
+++ app/src/main/java/com/example/fitquest/pages/HomePage.kt
@@ -6,15 +6,24 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,11 +38,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
 import com.example.fitquest.AuthState
 import com.example.fitquest.AuthViewModel
@@ -51,8 +65,6 @@ import com.google.firebase.database.database
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
-
-
 
     TopAndBottomAppBar(
     contents = { HomePageContents(modifier,navController,authViewModel) },
@@ -97,139 +109,296 @@ fun HomePageContents(modifier: Modifier = Modifier, navController: NavController
         }
     }
 
-
-    // display content if the userProfile is not null
     userProfile?.let { profile ->
-
         Column(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxSize()
-                .background(com.example.fitquest.ui.theme.verticalGradientBrush)
-                .padding(30.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+//                .background(Color.DarkGray)
+                .padding(
+                    start = 16.dp,
+//                    top = screenHeightDp.dp / 7 - 15.dp,
+//                    top = paddingValues.calculateTopPadding(),
+                    top = 0.dp,
+                    end = 16.dp,
+                    bottom = 0.dp
+                )
+                .verticalScroll(rememberScrollState())
         ) {
+//            Row(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(bottom = 16.dp),
+//                horizontalArrangement = Arrangement.SpaceBetween,
+//                verticalAlignment = Alignment.CenterVertically
+//            )
+//            {
+//                Text("FitQuest", fontSize = 32.sp, fontWeight = FontWeight.Bold, color = Color(0xFFFF6D00))
+//
+//
+//                //Plan is to make the circle the pfp but for now i just put the username in there
+//                Box(
+//                    modifier = Modifier
+//                        .size(100.dp)
+//                        .clip(CircleShape)
+//                        .background(Color.Gray),
+//                    contentAlignment = Alignment.Center
+//                ) {
+//                    Text(profile.username, fontSize = 20.sp, color = Color.White) //profile username
+//                }
+//            }
 
+//            // Display XP Progress Bar probably dont need this
+//            Text("XP", color = Color.White, fontSize = 14.sp)
+//            LinearProgressIndicator(
+//                progress = 0.7f,
+//                color = Color(0xFFFF6D00),
+//                trackColor = Color.LightGray,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(8.dp)
+//                    .padding(vertical = 8.dp)
+//            )
+
+
+            // This displays the streak
+            // i think the future plan is to have a fire emoji or something around it
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = "STREAK",
-                    color = grayWhite,
+                    color = Color.White,
                     fontWeight = FontWeight.Light,
                     fontSize = 18.sp
                 )
                 Spacer(modifier = Modifier.width(8.dp))
-
                 Text(
                     text = profile.streak.streak.toString(),
-                    color = grayWhite,
+                    color = Color.White,
                     fontSize = 18.sp,
                     fontWeight = FontWeight.Bold
                 )
-
             }
-            Spacer(modifier = Modifier.height(16.dp))
+            val database = Firebase.database
+            val myRef = database.getReference("Users")
 
-            // Logging
-            Button(
-                onClick = { navController.navigate("logging") },
-                colors = ButtonDefaults.buttonColors(containerColor = transparent),
-                enabled = authState.value != AuthState.Loading,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(55.dp),
-//                .border(width = 5.dp, color = Color(0xFFD58D18)),
-                shape = RoundedCornerShape(size = 25.dp),
-                border = BorderStroke(4.5.dp, brightOrange)
 
+            Column(
             ) {
-                Text(
-                    text = "Log Workout",
-                    color = brightOrange,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Text("Daily", fontSize = 32.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                }
+
+
+                Column {
+                    LazyRow {
+                        items(1) { index ->
+                            DailyBox(
+                                name = " ",
+                                title = "My Title",
+                                text = "This is some text in the box.",
+                                workouts = " ",
+                                onClick = {
+
+                                }
+                            )
+                            DailyBox(
+                                name = " ",
+
+                                title = "My Title",
+                                text = "This is some text in the box.",
+                                workouts = " ",
+                                onClick = {
+
+                                }
+                            )
+                            DailyBox(
+                                name = " ",
+                                title = "My Title",
+                                text = "This is some text in the box.",
+                                workouts = " ",
+                                onClick = {
+
+                                }
+                            )
+                        }
+                    }
+
+                }
+                Text(text = "hello")
+
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically)
+                {
+                    Text("Weekly", fontSize = 32.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+
+                Column {
+                    LazyRow {
+                        items(1) { index ->
+                            WeeklyBox(
+                                name = " ",
+                                title = "My Title",
+                                text = "This is some text in the box.",
+                                workouts = " ",
+                                onClick = {
+
+                                }
+                            )
+                            WeeklyBox(
+                                name = " ",
+
+                                title = "My Title",
+                                text = "This is some text in the box.",
+                                workouts = " ",
+                                onClick = {
+
+                                }
+                            )
+                            WeeklyBox(
+                                name = " ",
+                                title = "My Title",
+                                text = "This is some text in the box.",
+                                workouts = " ",
+                                onClick = {
+
+                                }
+                            )
+                        }
+                    }
+                }
+
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Recommended", fontSize = 32.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                    Spacer(modifier = Modifier.width(20.dp))
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp)
+                ) {
+                }
+                Column {
+                    ReccomendedBox(
+                        name = "Arnold Beefcake",
+
+                        title = "T-800 (muscles only!!)",
+                        text = "This is some text in the box.",
+                        workouts = "Upperbody, Lowerbody",
+                        onClick = {
+
+                        }
+                    )
+                    ReccomendedBox(
+                        name = "Emily LoveCraft",
+                        title = "Emily's gym Time!",
+                        text = "Test Test Test",
+                        workouts = "Core workout, Cardio",
+                        onClick = {
+
+                        }
+                    )
+                    ReccomendedBox(
+                        name = "Dave Rubin",
+                        title = "Training to Battle Psycho",
+                        text = "Test",
+                        workouts = "Core workout, Calisthenics, Stretching",
+                        onClick = {
+                        }
+                    )
+                }
+                //RandomChildDisplay()
             }
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Store
-            Button(
-                onClick = { navController.navigate("store") },
-                colors = ButtonDefaults.buttonColors(containerColor = transparent),
-                enabled = authState.value != AuthState.Loading,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(55.dp),
-//                .border(width = 5.dp, color = Color(0xFFD58D18)),
-                shape = RoundedCornerShape(size = 25.dp),
-                border = BorderStroke(4.5.dp, brightOrange)
-
-            ) {
-                Text(
-                    text = "Store",
-                    color = brightOrange,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Stats
-            Button(
-                onClick = { navController.navigate("stats") },
-                colors = ButtonDefaults.buttonColors(containerColor = transparent),
-                enabled = authState.value != AuthState.Loading,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(55.dp),
-//                .border(width = 5.dp, color = Color(0xFFD58D18)),
-                shape = RoundedCornerShape(size = 25.dp),
-                border = BorderStroke(4.5.dp, brightOrange)
-
-            ) {
-                Text(
-                    text = "Stats",
-                    color = brightOrange,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // For You
-            Button(
-                onClick = { navController.navigate("foryou") },
-                colors = ButtonDefaults.buttonColors(containerColor = transparent),
-                enabled = authState.value != AuthState.Loading,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(55.dp),
-//                .border(width = 5.dp, color = Color(0xFFD58D18)),
-                shape = RoundedCornerShape(size = 25.dp),
-                border = BorderStroke(4.5.dp, brightOrange)
-
-            ) {
-                Text(
-                    text = "For You",
-                    color = brightOrange,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-
-
-            TextButton(
-                onClick = { authViewModel.signout() }) {
-                Text(text = "Sign Out", color = Color.Red)
-            }
-
-
         }
     }
+}
 
+
+@Composable
+fun PopupBox(popupWidth: Float, popupHeight:Float, showPopup: Boolean, onClickOutside: () -> Unit, content: @Composable() () -> Unit) {
+
+    if (showPopup) {
+        // full screen background
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Green)
+                .zIndex(10F),
+            contentAlignment = Alignment.Center
+        ) {
+            // popup
+            Popup(
+                alignment = Alignment.Center,
+                properties = PopupProperties(
+                    excludeFromSystemGesture = true,
+                ),
+                // to dismiss on click outside
+                onDismissRequest = { onClickOutside() }
+            ) {
+                Box(
+                    Modifier
+                        .width(popupWidth.dp)
+                        .height(popupHeight.dp)
+                        .background(Color.White)
+                        .clip(RoundedCornerShape(4.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    content()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PopupButton() {
+    var showDialog by remember { mutableStateOf(false) }
+
+    Button(onClick = { showDialog = true },
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.Transparent
+        )
+    ) {
+        Text("Show Popup")
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text("Popup Title") },
+            text = { Text("This is a popup message.") },
+            confirmButton = {
+                Button(onClick = { showDialog = false }) {
+                    Text("OK")
+                }
+                Button(onClick = { showDialog = false }) {
+                    Text("OK")
+                }
+            }
+        )
+    }
     // This modify the device's back button to function like a the device's home button
     // pressing it will make it minimize the app in stead of shutting it down completely
+
+    val context = LocalContext.current
+
     BackHandler(enabled = true) {
         val intent = Intent(Intent.ACTION_MAIN)
         intent.addCategory(Intent.CATEGORY_HOME)
